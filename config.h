5f4c2d9ae80522e000c07b8fe8026f198146e05f
Gustavo Meireles <gugatpm@gmail.com>
2023-08-15 23:23:25 +0100

    font

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //static char *font = "CaskaydiaCove Nerd Font Mono:pixelsize=11:antialias=true:autohint=true";
-static char *font = "CaskaydiaCove Nerd Font Mono:pixelsize=18.5:antialias=true:autohint=true";
+static char *font = "CaskaydiaCove Nerd Font Mono:pixelsize=13:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
