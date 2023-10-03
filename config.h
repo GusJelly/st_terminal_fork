e8dc6b84282df921285507f2248da43bda786b8e
Gustavo Meireles <gugatpm@gmail.com>
2023-10-03 02:51:03 +0100

    New patches lmao

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 //static char *font = "CaskaydiaCove Nerd Font Mono:pixelsize=11:antialias=true:autohint=true";
-static char *font = "CaskaydiaCove Nerd Font Mono:pixelsize=13:antialias=true:autohint=true";
+static char *font = "Hack Nerd Font Mono:pixelsize=13:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
