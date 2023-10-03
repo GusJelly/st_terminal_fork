b6d71ca15895d6d3a4941bde3595aad7140872ee
Gustavo Meireles <gugatpm@gmail.com>
2023-10-03 19:17:10 +0100

    Trying to fix shit idk I can't do it

--- config.def.h
+++ config.def.h
@@ -240,16 +240,16 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ Mod1Mask|ShiftMask,   XK_Page_Up,     zoom,           {.f = +1} },
-	{ Mod1Mask|ShiftMask,   XK_Page_Down,   zoom,           {.f = -1} },
+	{ Mod1Mask|ShiftMask,   XK_K,           zoom,           {.f = +1} },
+	{ Mod1Mask|ShiftMask,   XK_J,           zoom,           {.f = -1} },
 	{ Mod1Mask|ShiftMask,   XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ TERMMOD,              XK_K,           kscrollup,      {.i = 4} },
+	{ TERMMOD,              XK_J,           kscrolldown,    {.i = 4} },
 };
 
 /*
